8053228c1da2b7ade8af80bcd69f29906208e665
Ezekiel Halley <eze.halley@gmail.com>
2022-04-30 03:43:53 -0400

    Update

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,14 @@
 ﻿using Chip8;
-using Raylib_cs
 
 class Program
 {
     static void Main(string[] args)
     {
-        
-        /*
         var chip8 = new Emulator();
         chip8.Initialize();
         chip8.Load("D:/Code/C Sharp/COSC439_Chip8_Project/chip8-roms/games/Pong (1 player).ch8");
         chip8.AdjustDisplay(15, "Chip8");
         chip8.Process();
-        */
+        
     }
 }
